ee78b50ab95b7d3e23f0f09e33da3329b006294c
Andrew Mao <mizzao@gmail.com>
2013-02-05 17:43:29 -0500

    started some gui elements

--- turkserver/src/main/java/edu/harvard/econcs/turkserver/server/gui/ControlPanel.java
+++ turkserver/src/main/java/edu/harvard/econcs/turkserver/server/gui/ControlPanel.java
@@ -1,5 +1,7 @@
 package edu.harvard.econcs.turkserver.server.gui;
 
+import java.awt.FlowLayout;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -8,6 +10,7 @@ public class ControlPanel extends JPanel {
 	private static final long serialVersionUID = 898019683117861450L;
 
 	public ControlPanel() {
+		setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		add(new JLabel("Control Panel"));
 		
--- turkserver/src/main/java/edu/harvard/econcs/turkserver/server/gui/TSTabbedPanel.java
+++ turkserver/src/main/java/edu/harvard/econcs/turkserver/server/gui/TSTabbedPanel.java
@@ -9,18 +9,28 @@ public class TSTabbedPanel extends JPanel {
 
 	private static final long serialVersionUID = -3363135146104711554L;
 
-	private JTabbedPane tabbedPane;
+	private JTabbedPane tabbedPane;	
 	
 	public TSTabbedPanel() {
 		super(new GridLayout(1, 1));
 		
-		tabbedPane = new JTabbedPane();
-		
-		tabbedPane.addTab("Control", new ControlPanel());
-		
-		add(tabbedPane);
 		// Enable scrolling tabs.
 		tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+		
+		tabbedPane = new JTabbedPane();
+		add(tabbedPane);
+		
+		ControlPanel controlPanel = new ControlPanel();
+		tabbedPane.addTab("Control", null, controlPanel, null);		
+		
+		WorkerPanel workerPanel = new WorkerPanel();
+		tabbedPane.addTab("Workers", null, workerPanel, null);
+		
+		ExperimentsPanel experimentsPanel = new ExperimentsPanel();
+		tabbedPane.addTab("Experiments", null, experimentsPanel, null);		
+		
+		MaintenancePanel maintenancePanel = new MaintenancePanel();
+		tabbedPane.addTab("Maintenance", null, maintenancePanel, null);				
 	}
 	
 	public void addPanel(String title, JPanel panel) {
